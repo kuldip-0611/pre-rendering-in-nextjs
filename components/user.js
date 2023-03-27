afb8d948bf207a6d7e4db7a5741d0426ed334a1a
kuldip-zignuts <kuldipp@zignuts.com>
2023-03-27 15:37:57 +0530

    pre-rendering using api

---
+++ components/user.js
@@ -0,0 +1,9 @@
+const User = ({user})=>{
+    return (
+        <>
+        <h1>{user.name}</h1>
+        <h1>{user.id}</h1>
+        </>
+    )
+}
+export default User
---
+++ pages/users.js
@@ -0,0 +1,28 @@
+import User from "@/components/user";
+
+const Users = ({users})=>{
+    return (
+        <>
+            <h1>users List Is</h1>
+            {
+                users.map(user=>{
+                    return ( <User user={user} />)
+                })
+            }
+        </>
+    )
+}
+export default Users;
+
+export async function getStaticProps(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+
+    console.log(data);
+    return {
+        props:{
+            users:data
+        }
+    }
+
+} 
